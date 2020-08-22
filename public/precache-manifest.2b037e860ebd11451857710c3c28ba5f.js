a71dd4c4166eaa6ee15cbe1dd4db52c6083923cf
Bk <bikaleg@gmail.com>
2020-08-22 19:20:09 -0400

    deploy react

---
+++ public/precache-manifest.2b037e860ebd11451857710c3c28ba5f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5749f84e723db1b21220f58b2edb9bb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a86f4a51c5cf7e6a8d5",
+    "url": "/static/css/main.f59176e3.chunk.css"
+  },
+  {
+    "revision": "7c663a817c17f2fb657f",
+    "url": "/static/js/2.1ff64ffb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ff64ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce731b25c745bc53634",
+    "url": "/static/js/3.dc8da572.chunk.js"
+  },
+  {
+    "revision": "d9936e688aa8d067aca1",
+    "url": "/static/js/4.26849958.chunk.js"
+  },
+  {
+    "revision": "4a86f4a51c5cf7e6a8d5",
+    "url": "/static/js/main.19165926.chunk.js"
+  },
+  {
+    "revision": "7987eecb59c19e5fdfc3",
+    "url": "/static/js/runtime-main.14534420.js"
+  },
+  {
+    "revision": "c1ab7e7c09b7f4dbd8c4aa9b15817eae",
+    "url": "/static/media/churchdress.c1ab7e7c.jpg"
+  },
+  {
+    "revision": "d28956925bbb4190c510aeb4f326f3eb",
+    "url": "/static/media/crosssign.d2895692.jpg"
+  },
+  {
+    "revision": "53abd5a5135875f858ac26972364abd1",
+    "url": "/static/media/kidsactivity.53abd5a5.jpg"
+  },
+  {
+    "revision": "9c72c029a267b5e98ed4dc76dc29a822",
+    "url": "/static/media/ourbelief.9c72c029.jpg"
+  },
+  {
+    "revision": "0beb89d10047aa765423108390294aae",
+    "url": "/static/media/palmsunday.0beb89d1.jpg"
+  },
+  {
+    "revision": "72a052addd3e52fe16dd348927e47e6a",
+    "url": "/static/media/staffs.72a052ad.jpg"
+  },
+  {
+    "revision": "1d281fef9f17b955257d7d2a30fc1199",
+    "url": "/static/media/starsema.1d281fef.jpg"
+  },
+  {
+    "revision": "6bf26226c75f023df300143a3a3d0280",
+    "url": "/static/media/whattoexpect.6bf26226.jpg"
+  },
+  {
+    "revision": "0a3ceb2c7d3b222e4ae29561cef7b812",
+    "url": "/static/media/worms-eyeview-of-green-trees-957024.0a3ceb2c.jpg"
+  }
+]);
